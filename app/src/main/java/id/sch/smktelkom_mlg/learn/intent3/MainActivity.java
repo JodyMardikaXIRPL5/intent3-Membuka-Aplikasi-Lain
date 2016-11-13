60310a71a81bd4aa9c7479362263b9543bce8422
Jody Mardika <jody_mardika_24rpl@student.smktelkom-mlg.sch.id>
2016-11-14 00:20:36 +0700

    Intent Camera (Photo) (perbaikan)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,15 +68,6 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
-
-    private void dialPhoneNumber(String phoneNumber) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel : " + phoneNumber));
-        if (intent.resolveActivity(getPackageManager()) != null)
-            startActivity(intent);
-    }
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public void capturePhoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
